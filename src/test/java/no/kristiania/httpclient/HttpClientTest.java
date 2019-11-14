2c05335082c5e63ac9a5b154e731bfbba73c9331
magomannen <magomed11@live.no>
2019-11-14 15:55:36 +0100

    Add first test and JUnit support

---
+++ src/test/java/no/kristiania/httpclient/HttpClientTest.java
@@ -0,0 +1,13 @@
+package no.kristiania.httpclient;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+
+    @Test
+    void shouldWork() {
+        assertEquals(6, 3 + 3);
+    }
+}
